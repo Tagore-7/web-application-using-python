4e4b0e259d7c32d02f2e939d14f15764c3856ce1
Tagore Rao Kosireddy <tagorerao01@gmail.com>
2024-08-10 20:31:19 +0000

    Dynamic Database-Driven Pages

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,5 +13,13 @@ def list_jobs():
   JOBS = load_jobs_from_db()
   return jsonify(JOBS)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job = job)
+
+  
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
