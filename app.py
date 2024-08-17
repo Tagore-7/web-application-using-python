4f4d91c9bb0802fc69b14b093de8eb44367e7f44
Tagore Rao Kosireddy <tagorerao01@gmail.com>
2024-08-17 23:02:42 +0000

    HTML Form creation for Applications

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,6 +21,12 @@ def show_job(id):
     return "Not Found", 404
   return render_template('jobpage.html', job = job)
 
-  
+@app.route("/job/<id>/apply")
+def apply_job(id):
+  data = request.args
+  return jsonify(data)
+
+
+
 if __name__ == "__main__":
   app.run(host = '0.0.0.0', debug = True)
