86541f9fb0173733069a86e147c32f80b674b0be
Tagore Rao Kosireddy <tagorerao01@gmail.com>
2024-08-18 23:09:53 +0000

    saving application to DB

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -21,10 +21,11 @@ def show_job(id):
     return "Not Found", 404
   return render_template('jobpage.html', job = job)
 
-@app.route("/job/<id>/apply")
+@app.route("/job/<id>/apply", methods=['post'])
 def apply_job(id):
-  data = request.args
-  return jsonify(data)
+  data = request.form
+  add_application_to_db(id , data)
+  return render_template("application_submitted.html", application = data)
 
 
 
--- database.py
+++ database.py
@@ -7,23 +7,11 @@ import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-# import pymysql
-
-# connection = pymysql.connect(
-#   host = "gateway01.us-east-1.prod.aws.tidbcloud.com",
-#   port = 4000,
-#   user = "2d1KPCHFnP4uEtv.root",
-#   password = "<PASSWORD>",
-#   database = "test",
-#   ssl_verify_cert = True,
-#   ssl_verify_identity = True,
-#   ssl_ca = "<CA_PATH>"
-# )
 
 engine = create_engine(db_connection_string, 
                    connect_args=  {
                         "ssl": {
-                          "ssl_ca": "C:\\Users\\TAGORE\\Downloads\\isrgrootx1.pem"
+                          "ssl_ca": "isrgrootx1.pem"
                         }
                       }
                       )
@@ -48,3 +36,13 @@ def load_job_from_db(id):
       return None
     else:
       return rows[0]._asdict()
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn: 
+    query = text("insert into applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education,:work_experience, :resume_url)")
+
+    conn.execute(query, {"job_id": job_id, "full_name": data['full_name'], "email": data['email'], "linkedin_url":data['linkedin_url'], "education":data['education'], "work_experience":data['work_experience'],
+
+"resume_url":data['resume_url']})
+    
